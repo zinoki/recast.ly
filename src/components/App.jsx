578659a6e3d631ea3a05de64bea365a9b0a13153
zinoki <zinouye@gmail.com>
2018-01-31 15:44:20 -0800

    we got verything show no the screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>search</em><Search /></h5></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em><VideoPlayer /></h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div><h5><em>videoList</em><VideoList /></h5></div>
       </div>
     </div>
   </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
-var VideoList = () => (
+var VideoList = (props) => (
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em><VideoListEntry /></h5></div>
   </div>
 );
 
@@ -17,3 +18,9 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+
+  // {props.data.map(video => 
+  //     <div><h5><em>videoListEntry</em><VideoListEntry video={video}/></h5></div>
+  //   )}
