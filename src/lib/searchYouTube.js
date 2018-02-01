424ddfdc690e427594d69a496400479a8252df5e
zinoki <zinouye@gmail.com>
2018-01-31 20:03:19 -0800

    everything passing except search bar

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,13 +1,20 @@
 var searchYouTube = (options, callback) => {
-  fetchData() {
-    $.ajax({
-    url:'https://www.googleapis.com/youtube/v3/search',
-    type:'GET',
-    success:function(data) {
-        console.log(data)
-      }
-    })
-  }
+  // fetchData() {
+  //   $.ajax({
+  //   url:'https://www.googleapis.com/youtube/v3/search',
+  //   type:'GET',
+  //   success:function(data) {
+  //       console.log(data)
+  //     }
+  //   })
+  // }
+  $.get('https://www.googleapis.com/youtube/v3/search', function(data){
+    console.log(data)
+  },{ 'key':window.YOUTUBE_API_KEY,
+      'maxResults': '5',
+      'part': 'snippet',
+      'q': 'surfing',
+      'type': ''})
 };
 
 window.searchYouTube = searchYouTube;
